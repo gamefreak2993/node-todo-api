bf0bd1cc62c5b2a31c885d24bfa262f403fc7f96
Mihai Vărșăndan <mihai@europasat.com>
2018-10-29 14:11:48 +0000

    add: post todos route and config mongoose

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express"),
+    bodyParser = require("body-parser");
+
+const { mongoose } = require("./db/mongoose"),
+    { Todo } = require("./models/todo"),
+    { User } = require("./models/user");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
