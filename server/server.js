32f91623b54ffb6539c9ce5d39b6961b15f786e4
Mihai Vărșăndan <mihai@europasat.com>
2018-10-30 11:23:15 +0000

    add: PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    _ = require("lodash");
 
 const { mongoose } = require("./db/mongoose"),
     { ObjectID } = require("mongodb"),
@@ -62,6 +63,32 @@ app.delete("/todos/:id", (req, res) => {
         if (!todo) {
             return res.status(404).send();
         };
+
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
+    });
+});
+
+app.patch("/todos/:id", (req, res) => {
+    const id = req.params.id,
+        body = _.pick(req.body, ["text", "completed"]);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    };
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
 
         res.send({todo});
     }).catch((err) => {
